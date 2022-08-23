5a725ab8e0c32ae4ded5d4156e3e183a25b3d16e
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2022-08-23 17:32:34 +0200

    Added all basic functionality.

--- README.md
+++ README.md
@@ -3,24 +3,46 @@ My first Rust project!
 ---
 It's a snake game running in the terminal. Move by typing one of the "wasd" keys and pressing enter. I used Robert Heaton's https://robertheaton.com/2018/12/02/programming-project-5-snake/ for inspiration and guidance.
 ---
+Manual:
+Move with `wasd` followed by `enter` key. 
+Terminate game early with `t` followed by `enter`
+
+use command line arguments before running to change board size and edge behaviour
+Formatted like: `[binary] [arg1] [arg2] [arg3]`
+`arg1 and arg2` are board width and height respectively (min 5, max 25 in either direction)
+`arg3` lets you change edge behaviour from "wrapping/teleporting" to being lethal "walls" by inserting `Wall`
+CLA are only read if you submit two or three args after the executable.
+For any argument that cannot be parsed it will revert to the default value (10*10 board with wrapping)
 
 TODO, roughly by order of importance:
 * functional
 - [x] Fix Snake collision detection. I broke it somewhere along the way 
 - [x] Make sure tail segments update properly upon apple eating
-- [ ] Make sure the apple can't appear in a snake-occupied location
-- [ ] Count score and display it 
-- [ ] Add option to change board size (probably by using CLA?)
-- [ ] Add option to change board edge behaviour (currently wraps by default, (CLA?))
+- [x] Make sure the apple can't appear in a snake-occupied location
+- [x] Add a min-max size to player-defined boards
+- [x] Count score and display it 
+- [x] Add option to change board size (probably by using CLA?)
+- [x] Add option to change board edge behaviour (currently wraps by default, (CLA?))
+- [x] Give player the option the exit the game early
 - [ ] Register moves with single button presses using `curses` library (or make it move automatically? Apparently really difficult using the terminal)
+- [ ] Variable apple score based on steps it took for player to reach it
+- [ ] 🍎🍎 End the game when a player fills the entire board with Snake. Currently loops endlessly trying to find a location for Apple 🍎🍎
+- [ ] 🧱 Let user switch edge behaviour with a single CLA instead of three 🧱
 
-* pure refactoring/cleaning up
-- [ ] Don't clear the board and recalculate every tick. Just update the necessary spaces (currently I set all spaces to empty and then insert the apple and snake accordingly)
+* refactoring
+- [x] Don't clear the board and recalculate every tick. Just update the necessary spaces (currently I set all spaces to empty and then insert the apple and snake accordingly)
+- [ ] Wrap CLA handling in a function
 - [ ] For the board, switch from 2D array with a set size, to a 1d vec with an indexer
-- [ ] Replace if-else piles with `match` statements where possible?
+- [ ] Move the structs and such into separate files?
+- [x] Replace if-else piles with `match` statements where possible?
 
 Changelog:
 - 22/08/2022 
   - First working version
-- 23/08/2022 
+- 23/08/2022 part 1:
   - FIXED: collision detection & tail segment updating
+- 23/08/2022 part 2 electric boogaloo:
+  - FIXED: new apple can no longer appear inside of the Snake
+  - ADDED: score counter, command line options for the player
+  - CHANGED: changed the way I handle board changes, substituted *most* `else-if` mountains with `match` hill
+  - (note: added pancurses to `[dependencies]` for later use)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 /* My first Rust project, for getting used to the language and syntax
- I used https://robertheaton.com/2018/12/02/programming-project-5-snake/ 
- by Robert Heaton for inspiration/guidance. These exercises are fun! */
+I used https://robertheaton.com/2018/12/02/programming-project-5-snake/
+by Robert Heaton for inspiration/guidance. These exercises are fun! */
 
-use std::collections::VecDeque; // Vec Deque lets you pop elements off the vector in reverse order. Important!
-use std::io;
 use rand::Rng;
+use std::collections::VecDeque; // Vec Deque lets you pop elements off the vector in reverse order. Important!
+use std::env;
+use std::io;
 
 // consts used for the snake direction
 const UP: i32 = 0;
@@ -16,24 +17,27 @@ const EMPTY: i32 = 0;
 const HEAD: i32 = 1;
 const TAIL: i32 = 2;
 const APPLE: i32 = 3;
+const PORTAL: bool = false;
+const WALL: bool = true;
 
 struct Snake {
-	head: (i32, i32, i32), 				// (x coord, y coord, lifetime of tail segments)
-	tail: VecDeque<(i32, i32, i32)>,	// (x coord, y coord, lifetime) of segment
-	direction: i32,						// what da snake doin?
+	head: (i32, i32, i32), // (x coord, y coord, lifetime of tail segments)
+	tail: VecDeque<(i32, i32, i32)>, // (x coord, y coord, lifetime) of segment
+	direction: i32,        // what da snake doin?
 }
 
 impl Snake {
+	// place the snake in the middle of the board
 	pub fn initialize(x: i32, y: i32) -> Self {
 		Self {
-			head: (x, y, 1),
+			head: (x / 2, y / 2, 1),
 			tail: VecDeque::new(),
 			direction: RIGHT,
 		}
 	}
 
 	// snake moves based on the value of direction
-	pub fn take_step(&mut self) {
+	pub fn take_step(&mut self, game: &mut Game) {
 		let mut flag = false;
 		self.tail.push_back(self.head);
 		match self.direction {
@@ -41,12 +45,10 @@ impl Snake {
 			RIGHT => self.head.0 += 1,
 			DOWN => self.head.1 += 1,
 			LEFT => self.head.0 -= 1,
-			_ => panic!("invalid direction"),
+			_ => panic!("invalid direction. How'd you do that?"),
 		}
 
 		// lower the "lifetime" of each tail segment by 1
-		// TODO: don't lower the lifetime on the tick an apple gets eaten
-		// Currently segments get added too late due to how segment lifetime works atm
 		for mut segment in &mut self.tail {
 			segment.2 -= 1;
 			if segment.2 == 0 {
@@ -56,98 +58,116 @@ impl Snake {
 
 		// if lifetime of tail segment reaches zero, remove oldest segment
 		if flag == true {
-			self.tail.pop_front();
+			let temp = self.tail.pop_front();
+			match temp {
+				Some(i) => game.board[i.0 as usize][i.1 as usize] = 0,
+				None => panic!("This should never happen"),
+			}
 		}
 	}
 
 	// take user input, change snake direction accordingly
 	// uses WASD + enter. only checks the first character put in
-	pub fn set_direction(&mut self) {
+	pub fn set_direction(&mut self, game: &Game) {
 		let mut input = String::new();
 		io::stdin().read_line(&mut input).unwrap();
 		let first_char = input.chars().next().unwrap();
-		if first_char == 'w' && self.direction != DOWN {
-			self.direction = UP;
-		} else if first_char == 'd' && self.direction != LEFT {
-			self.direction = RIGHT;
-		} else if first_char == 's' && self.direction != UP {
-			self.direction = DOWN;
-		} else if first_char == 'a' && self.direction != RIGHT {
-			self.direction = LEFT;
+		match first_char {
+			'w' if self.direction != DOWN => self.direction = UP,
+			'd' if self.direction != LEFT => self.direction = RIGHT,
+			's' if self.direction != UP => self.direction = DOWN,
+			'a' if self.direction != RIGHT => self.direction = LEFT,
+			't' => game.end_game(),
+			_ => (),
 		}
 	}
 }
 
 // It's an apple
 struct Apple {
-	location: (i32, i32), 
+	location: (i32, i32),
 	_value: i32, // score value, CURRENTLY UNUSED. Maybe grant bonus points for speed
 }
 
 impl Apple {
-	// TODO: don't hardcode intial apple location. Probably don't need this function
-	pub fn initialize() -> Self {
+	// first apple always appears diagionally lower right from the snake head
+	pub fn initialize(x: i32, y: i32) -> Self {
 		Self {
-			location: (2, 6),
+			location: (x/2 + 1, y/2 + 1),
 			_value: 1,
 		}
 	}
 
 	// places new apple in random location on the board after eating.
-	// TODO: check if the selected location is already occupied by the snake
-	pub fn new_loc(&mut self, game: &Game){
-		self.location.0 = rand::thread_rng().gen_range(0..game.width);
-		self.location.1 = rand::thread_rng().gen_range(0..game.height);
+	pub fn new_loc(&mut self, game: &Game) {
+		loop {
+			self.location.0 = rand::thread_rng().gen_range(0..game.width);
+			self.location.1 = rand::thread_rng().gen_range(0..game.height);
+			// If the generated location is already occupied by a snake it tries a new location
+			// VERY INEFFICIENT and never ends looping if the player somehow fills the entier board with it's own body
+			if game.board[self.location.0 as usize][self.location.1 as usize] == 0 {
+				break;
+			}
+		}
 	}
 }
 
 struct Game {
 	width: i32,
 	height: i32,
-	board: [[i32; 256]; 256], // board of 256 by 256 array.
-	// TODO: make this a 1d vec with div/mod nonsense
+	board: [[i32; 25]; 25], // board of 256 by 256 array.
+	                          // TODO: make this a 1d vec with div/mod nonsense
+	edge_state: bool,
+	score: i32,
 }
 
 impl Game {
 	// initialize the board, completely empty
-	pub fn initialize(width: i32, height: i32) -> Self {
+	pub fn initialize(width: i32, height: i32, edge_state: bool) -> Self {
 		Self {
 			width,
 			height,
-			board: [[0; 256]; 256],
-		}
-	}
-
-	// Clears the board before checking so the update() function can update the board properly
-	// TODO: as opposed to resetting the board entirely, remove this function
-	// And set the tile behind the snake's fluffy tail to 0 every tick
-	pub fn clear(&mut self) {
-		for (_x, row) in self.board.iter_mut().enumerate() {
-			for (_y, col) in row.iter_mut().enumerate() {
-				*col = 0;
-			}
+			board: [[0; 25]; 25],
+			edge_state,
+			score: 0,
 		}
 	}
 
 	// Check if snek goes OOB, eats, or treads on itself
 	pub fn check_state(&mut self, snake: &mut Snake, apple: &mut Apple) {
-		// The snake wraps to the other side when going OOB.
-		// Conveniently lets me skip bounds checking for now. :)
-		// TODO: add option to make walls lethal
-		if snake.head.0 < 0 {
-			snake.head.0 = self.width - 1;
-		} else if snake.head.0 >= self.width {
-			snake.head.0 = 0;
-		} else if snake.head.1 < 0 {
-			snake.head.1 = self.height - 1;
-		} else if snake.head.1 >= self.height {
-			snake.head.1 = 0;
+
+		// Snake wraps around the edges when the edges are portals (default)
+		if self.edge_state == PORTAL {
+			if snake.head.0 < 0 {
+				snake.head.0 = self.width - 1;
+			} else if snake.head.0 >= self.width {
+				snake.head.0 = 0;
+			} else if snake.head.1 < 0 {
+				snake.head.1 = self.height - 1;
+			} else if snake.head.1 >= self.height {
+				snake.head.1 = 0;
+			} // How do I turn this into a match expression??
+		}
+
+		// Snake dies when running into an edge when the edges are walls
+		if self.edge_state == WALL {
+			if snake.head.0 < 0 {
+				self.end_game();
+			} else if snake.head.0 >= self.width {
+				self.end_game();
+			} else if snake.head.1 < 0 {
+				self.end_game();
+			} else if snake.head.1 >= self.height {
+				self.end_game();
+			}
 		}
 
 		// did the snake succeed in eating a healthy vegetarian diet?
 		if snake.head.0 == apple.location.0 && snake.head.1 == apple.location.1 {
 			snake.head.2 += 1; // EXTEND!
+			self.score += 1;
 			apple.new_loc(&self);
+			// increase the lifetime of all existing tail segments by 1 so the snake extends on the next tick
 			for segment in &mut snake.tail {
 				segment.2 += 1;
 			}
@@ -156,8 +176,7 @@ impl Game {
 		// Don't tread on yourself. Doing that ends the game.
 		for segment in &snake.tail {
 			if snake.head.0 == segment.0 && snake.head.1 == segment.1 {
-				println!("Game over!");
-				std::process::exit(0);
+				self.end_game();
 			}
 		}
 	}
@@ -178,17 +197,20 @@ impl Game {
 		while y < (self.height + 1) {
 			while x < (self.width + 1) {
 				if x == -1 || x == self.width || y == -1 || y == self.height {
-					print!("■");
+					if self.edge_state == WALL {
+						print!("█");
+					}
+					if self.edge_state == PORTAL {
+						print!("░");
+					}
 				} else {
-					if self.board[x as usize][y as usize] == EMPTY {
-						print!(" ");
-					} else if self.board[x as usize][y as usize] == HEAD {
-						print!("X");
-					} else if self.board[x as usize][y as usize] == TAIL {
-						print!("O");
-					} else if self.board[x as usize][y as usize] == APPLE {
-						print!("@");
-					} 
+					match self.board[x as usize][y as usize] {
+						EMPTY => print!(" "),
+						HEAD => print!("X"),
+						TAIL => print!("O"),
+						APPLE => print!("@"),
+						_ => panic!("This should never happen."),
+					}
 				}
 				x += 1;
 			}
@@ -196,24 +218,59 @@ impl Game {
 			x = -1;
 			y += 1;
 		}
+		println!("Score {}", self.score);
+	}
+
+	// Function that ends the game and displays the player's score
+	fn end_game(&self) {
+		println!("Game over!");
+		println!("Your score: {}", self.score);
+		std::process::exit(0);
 	}
 }
 
 fn main() {
-	let mut snake = Snake::initialize(5, 5);
-	let mut game = Game::initialize(10, 10);
-	let mut apple = Apple::initialize();
+	let args: Vec<String> = env::args().collect();
+	let width: i32;
+	let height: i32;
+	let edge_state: bool;
 
-	// Bunch of TODOs: let game progress with a single keypress instead of having to press enter every time
-	// Maybe make it progress with time?? :)
-	// Add score display.
-	// Add option to determine board size through command line args
+	// Parsing the command line args, if any.
+	if args.len() == 3 || args.len() == 4 {
+		width = match args[1].parse::<i32>() {
+			Ok(number) if number >= 5 && number <= 25 => number,
+			_ => 10,
+		};
+		height = match args[2].parse::<i32>() {
+			Ok(number) if number >= 5 && number <= 25 => number,
+			_ => 10,
+		};
+		if args.len() == 4 {
+			if args[3] == "Wall" {
+				edge_state = WALL;
+			} else {
+				edge_state = PORTAL;
+			}
+		} else {
+			edge_state = PORTAL;
+		}
+	} else {
+		width = 10;
+		height = 10;
+		edge_state = PORTAL;
+	}
+
+	// intialize state of structs we'll be using based on user input
+	let mut game = Game::initialize(width, height, edge_state);
+	let mut snake = Snake::initialize(width, height);
+	let mut apple = Apple::initialize(width, height);
+
+	// endlessly loop until Game::end_game() gets called by another function
 	loop {
-		game.clear();
 		game.check_state(&mut snake, &mut apple);
 		game.update(&snake, &apple);
 		game.render();
-		snake.set_direction();
-		snake.take_step();
+		snake.set_direction(&game);
+		snake.take_step(&mut game);
 	}
 }
